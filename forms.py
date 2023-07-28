f14d012d9ab4528f02ded219fb6df3e4463ab225
Kirra Hyde <kirra.hyde@gmail.com>
2023-07-28 18:40:53 -0400

    add validations

--- forms.py
+++ forms.py
@@ -1,17 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,BooleanField,RadioField,TextAreaField
+from wtforms.validators import AnyOf
 
 """Forms for adopt app."""
 
 class AddPetForm(FlaskForm):
     name = StringField('name')
-    species = StringField('species')
+    species = StringField('species',
+        validators=[AnyOf(["cat", "dog", "porcupine"])]
+    )
     photo_url = StringField('photo_url')
     age = RadioField(
         choices = [
             ('baby','Baby'),
-                ('young','Young'),
-                ('adult','Adult'),
-                ('senior','Senior')]
+            ('young','Young'),
+            ('adult','Adult'),
+            ('senior','Senior')
+        ],
+        validators=[AnyOf(["baby", "young", "adult", "senior"])]
     )
     notes = TextAreaField('notes')
